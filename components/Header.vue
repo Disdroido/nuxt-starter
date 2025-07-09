0474cf1ff77b57a9dc03d0476b837444265ee523
Disdroido <disdroido@gmail.com>
2025-07-09 17:08:08 +1000

    made it work

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <nav>
+      <a v-for="link in links" :href="link.data.url" :key="link.id">
+        {{ link.data.label }}
+      </a>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { fetchEntries } from '@builder.io/sdk-vue';
+
+const links = ref([]);
+
+const { data: navLinks } = await useAsyncData('builderDataNavLinks', () =>
+  fetchEntries({
+    model: 'nav-link',
+    apiKey: '25becc87cdbf4edab9eccb09d448af92',
+  })
+);
+
+links.value = navLinks.value || [];
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  extends: ['shadcn-docs-nuxt'],
-  i18n: {
-    defaultLocale: 'en',
-    locales: [
-      {
-        code: 'en',
-        name: 'English',
-        language: 'en-US',
-      },
-    ],
-  },
   compatibilityDate: '2024-07-06',
+  modules: ['@builder.io/sdk-vue/nuxt'],
 });
