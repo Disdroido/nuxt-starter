21c216fc1e36485ae45552486bfc7f435ac9c6f7
Disdroido <disdroido@gmail.com>
2025-07-10 13:44:41 +1000

    Update builder-components.client.ts

--- plugins/builder-components.client.ts
+++ plugins/builder-components.client.ts
@@ -10,7 +10,7 @@ import CustomHeader from "~/components/builder/CustomHeader.vue";
 
 export default defineNuxtPlugin(() => {
   // Register Hero Section
-  register(HeroSection, {
+  register('HeroSection', {
     name: "Hero Section",
     inputs: [
       {
@@ -44,7 +44,7 @@ export default defineNuxtPlugin(() => {
   });
 
   // Register Feature Card
-  register(FeatureCard, {
+  register('FeatureCard', {
     name: "Feature Card",
     inputs: [
       {
@@ -73,7 +73,7 @@ export default defineNuxtPlugin(() => {
   });
 
   // Register CTA Section
-  register(CTASection, {
+  register('CTASection', {
     name: "CTA Section",
     inputs: [
       {
@@ -107,7 +107,7 @@ export default defineNuxtPlugin(() => {
   });
 
   // Register Testimonial Card
-  register(TestimonialCard, {
+  register('TestimonialCard', {
     name: "Testimonial Card",
     inputs: [
       {
@@ -143,7 +143,7 @@ export default defineNuxtPlugin(() => {
   });
 
   // Register Stats Section
-  register(StatsSection, {
+  register('StatsSection', {
     name: "Stats Section",
     inputs: [
       {
@@ -213,7 +213,7 @@ export default defineNuxtPlugin(() => {
   });
 
   // Register Custom Header
-  register(CustomHeader, {
+  register('CustomHeader', {
     name: "Custom Header",
     inputs: [
       {
